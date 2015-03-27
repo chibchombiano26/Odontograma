26e3ecb70988a85c10332b18dd7d2335ae9f213a
Jose Douglas Ramirez E <chibchombiano26@hotmail.com>
2015-03-27 13:58:56 -0500

    Ejemplo winjs

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'winjs', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -65,7 +65,18 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         controller: 'PlaylistCtrl'
       }
     }
+  })
+
+.state('app.exampleListView', {
+    url: "/exampleListView",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/examples/listView.Html",
+        controller: 'listViewSampleCtrl'
+      }
+    }
   });
+
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/playlists');
 });
---
+++ www/js/examples/listView.js
@@ -0,0 +1,48 @@
+angular.module('starter')
+.controller("listViewSampleCtrl", function ($scope) {
+    var Mode = {
+        editMode: {
+            text: "edit",
+            selectionMode: "multi",
+            tapBehavior: "toggleSelect"
+        },
+        readOnly: {
+            text: "readonly",
+            selectionMode: "none",
+            tapBehavior: "none"
+        }
+    };
+
+    $scope.items = [
+        { title: "Marvelous Mint", text: "Gelato", picture: "/images/fruits/60Mint.png" },
+        { title: "Succulent Strawberry", text: "Sorbet", picture: "/images/fruits/60Strawberry.png" },
+        { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: "/images/fruits/60Banana.png" },
+        { title: "Lavish Lemon Ice", text: "Sorbet", picture: "/images/fruits/60Lemon.png" },
+        { title: "Creamy Orange", text: "Sorbet", picture: "/images/fruits/60Orange.png" },
+        { title: "Very Vanilla", text: "Ice Cream", picture: "/images/fruits/60Vanilla.png" },
+        { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: "/images/fruits/60Banana.png" },
+        { title: "Lavish Lemon Ice", text: "Sorbet", picture: "/images/fruits/60Lemon.png" }
+    ];
+
+    $scope.addItem = function () {
+        $scope.items.splice(0, 0, {
+            title: "Marvelous Mint",
+            text: "Gelato",
+            picture: "/images/fruits/60Mint.png"
+        });
+    };
+
+    $scope.removeItem = function () {
+        // Remove the items that are selected
+        for (var i = $scope.selection.length - 1; i > -1; i--) {
+            $scope.items.splice($scope.selection[i], 1);
+        }
+    };
+
+    $scope.toggleEditMode = function () {
+        $scope.mode = ($scope.mode === Mode.readOnly) ? Mode.editMode : Mode.readOnly;
+    };
+
+    $scope.mode = Mode.readOnly;
+    $scope.selection = [];
+});
