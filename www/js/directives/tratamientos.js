403ad49015b093b77e7152f168b8847d3670f587
Jose douglas ramirez <chibchombiano26@hotmail.com>
2015-03-28 20:12:49 -0500

    tratamientos

--- www/js/directives/tratamientos.js
+++ www/js/directives/tratamientos.js
@@ -10,6 +10,8 @@ angular.module('starter')
 		controller : function($scope, fuenteConverterService){
 			var item = $scope.item;
 			item.Fuente = fuenteConverterService.convertirFuente(item.Fuente);
+			if(item.ColorAdicional !== null) { item.ColorAdicional = '#' + item.ColorAdicional.toString(16); }
+			if(item.Color !== null) { item.Color = '#' + item.Color.toString(16); }
 		}
 	};
 }])
