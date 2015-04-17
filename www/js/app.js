d31e4975f4391aa99e5b97ca73d501d97c4d7fe4
chibchombiano@outlook.com <chibchombiano@outlook.com>
2015-04-17 16:54:57 -0500

    Inicio push

--- www/js/app.js
+++ www/js/app.js
@@ -4,10 +4,11 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'winjs', 'ngjsColorPicker', 'ngCordova', 'starter.hefesoft.azure'])
+angular.module('starter', ['ionic', 'winjs', 'ngjsColorPicker', 'ngCordova'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $rootScope) {
   $ionicPlatform.ready(function() {
+    $rootScope.$broadcast('Dispositivo listo');
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,58 @@
 angular.module('starter')
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $rootScope) {
+
+  var androidConfig = {
+    "senderID": "505952414500",
+  };
  
+	$scope.$on("'Dispositivo listo'", function(event, args){		
+		registerAndroid();        
+	});
+
+	$rootScope.$on('$cordovaPush:notificationReceived', function(event, notification) {
+      switch(notification.event) {
+        case 'registered':
+          if (notification.regid.length > 0 ) {
+            alert('registration ID = ' + notification.regid);
+          }
+          break;
+
+        case 'message':
+          // this is the actual push notification. its format depends on the data model from the push server
+          alert('message = ' + notification.message + ' msgCount = ' + notification.msgcnt);
+          break;
+
+        case 'error':
+          alert('GCM error = ' + notification.msg);
+          break;
+
+        default:
+          alert('An unknown GCM event has occurred');
+          break;
+      }
+    });
+
+	function registerAndroid(){
+		var deviceInformation = ionic.Platform.device();
+        var isAndroid = ionic.Platform.isAndroid();
+
+        if(isAndroid){
+			$cordovaPush.register(androidConfig).then(function(result) {
+		      	console.log(result);
+		    }, function(err) {
+		      	console.log(err);
+		    })
+		}
+	}
+
+	function inicializarTimer(){
+		$timeout(function() {
+	     	registerAndroid();
+	  	}, 10000);
+	}
+
+	inicializarTimer();
+
 })
 
